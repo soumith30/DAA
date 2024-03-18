7b937eb8a5b16bf04a54268146e92b35b620a0d4
soumith30 <124856470+soumith30@users.noreply.github.com>
2024-03-18 17:22:33 +0530

    Create 18 - prime series.cpp

---
+++ 18 - prime series.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,j,c;
+	printf("enter the range:");
+	scanf("%d",&n);
+	for(i=2;i<=n;i++)
+	{
+		c=0;
+		for(j=1;j<=i;j++)
+		{
+			if(i%j==0)
+			{
+				c+=1;
+			}
+		}
+		if(c<=2){
+			printf("%d ",i);
+		}
+	}
+	return 0;
+}
